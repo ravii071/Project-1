4e0adf2c7406cb99d3e9c7905d1df809c2500d76
Ravi Darji <rd07102002@gmail.com>
2023-08-21 21:57:51 +0530

    Day 4 completed

--- lib/login.dart
+++ lib/login.dart
@@ -6,12 +6,56 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-          child: Text(
-        "Login",
-        style: TextStyle(
-            fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold),
-      )),
+      color: Colors.white,
+      // Center(
+      // child: Text(
+      //   "Login",
+      //   style: TextStyle(
+      //       fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold),
+      // ),
+
+      child: Column(
+        children: [
+          Image.asset("assets/images/login_image.png"),
+          SizedBox(height: 20.0),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter password",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    print("Hyyy");
+                  },
+                  style: TextButton.styleFrom(),
+                  child: Text("Login"),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:first_app/login.dart';
 import 'package:first_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
 import 'pages/home_page.dart';
 
 void main() {
@@ -18,7 +20,11 @@ class Myapp extends StatelessWidget {
     return MaterialApp(
       // home: Homepage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.purple),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: "/home",
       routes: {
